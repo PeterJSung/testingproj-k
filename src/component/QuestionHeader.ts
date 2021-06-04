a5918d1e58c0a5a9593e8a7faa80f39b6e26ef23
PeterJSung <ivsinji746@naver.com>
2021-06-04 18:53:07 +0900

    backup

--- src/component/QuestionHeader.ts
+++ src/component/QuestionHeader.ts
@@ -14,12 +14,12 @@ const QuestionHeader = (data: QuestionHeaderInfo) => {
   const second = extractData<number>(data, "second", 0);
   const score: number = extractData<number>(data, "score", 0);
   return `
-    <div class="question-header">
-      <div>남은시간</div><div id="timer-value">${second}</div>
-    </div>
-    <div>
-      <div>점수:</div><div id="score-value">${score}</div>
-    </div>
+      <div>
+        <span>남은시간: </span><span id="timer-value">${second}</span>
+      </div>
+      <div>
+        <span>점수: </span><span id="score-value">${score}</span>
+      </div>
   `;
 };
 
